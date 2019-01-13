a358048c8af4b7e5e867a875e59b2b3af37ef2e8
unknown <hanhhip999@gmail.com>
2019-01-13 15:07:34 +0700

    !

--- baiTapSlide7/smartPhone/src/smartphone/SmartPhone.java
+++ baiTapSlide7/smartPhone/src/smartphone/SmartPhone.java
@@ -11,11 +11,43 @@ package smartphone;
  */
 public class SmartPhone {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private int sim;
+    private boolean hasCam;
+    
+    public SmartPhone() {
+        super();
     }
     
+    public SmartPhone(int sim, boolean hasCam, int id, String name, int quantity, int price) {
+        super(id, name, price, quantity);
+        this.sim = sim;
+        this.hasCam = hasCam;
+    }
+    
+    public int getSim() {
+        return sim;
+        
+    }
+    
+    public void setSim( int sim) {
+        this.sim=sim;
+    }
+    
+    public boolean getHasCam() {
+        return true;
+    }
+    
+    public void setHasCam(boolean hasCam) {
+        this.hasCam = hasCam;
+    }
+    @Override
+    public void addNew(){
+        super.addNew();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap hasCame: (1/0)");
+        int choice = sc.nextInt();
+        this.hasCamera = choice == 1 ? true : false;
+        System.out.println("Nhap sim: ");
+        int sim = sc.nextInt();
+    }
 }
