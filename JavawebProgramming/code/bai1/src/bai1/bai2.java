8e0672072f5d81181eed03aa1b3a348232c048d5
unknown <hanhhip999@gmail.com>
2019-02-15 12:41:14 +0700

    h

---
+++ JavawebProgramming/code/bai1/src/bai1/bai2.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class bai2  {
+    
+    
+    public static int convertNumToString(int num){
+    
+    
+        int s = 0;
+            if (s==1) {
+                System.out.println("mot");
+            }
+            if (s==2) {
+                System.out.println("hai");
+            }
+            if (s==3) {
+                System.out.println("ba");
+            }
+            if (s==4) {
+                System.out.println("bon");
+            }
+            if (s==5) {
+                System.out.println("nam");
+            }
+            if (s==6) {
+                System.out.println("sau");
+            }
+            if (s==7) {
+                System.out.println("bay");
+            }
+            if (s==8) {
+                System.out.println("tam");
+            }
+            if (s==9) {
+                System.out.println("chin");
+            }
+            
+        return s;
+	
+}
+    public static int chuyendoiHangthanhChuoi (int num){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap so n");
+            int n = sc.nextInt();
+        String doc;
+        switch (n) {
+            case 1:
+                
+            
+        }
+        
+        return 0;
+        
+    }
+    
+    }
